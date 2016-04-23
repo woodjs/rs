a233928b08fbb24239abf35d22a42de496ddf046
bugong <1761869346@qq.com>
2016-04-23 22:26:11 +0800

    修改error_handling

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    //! panic!
+    //! 在std标准库中，存在一个枚举Option<T>
+    //! Option<T>代表着两种值
+    //! Option<T>有一个unwrap方法，从Option中取值
+    //! Some(T): An element of type T was found
+    //! None: No element was found
+
+    fn test_option(i: Option<&str>) {
+        // 对None值unwrap，会报错
+        let temp = i.unwrap();
+
+        println!("> current value is {:?}", temp);
+
+        if temp == "a" {
+            panic!("error message!");
+        }
+    }
+
+    let a = Some("a");
+    let b = None;
+
+    test_option(a);
+    test_option(b);
 }

3eb1ab594b2d4770ca1dfb46643a636a831b3c9b
bugong <1761869346@qq.com>
2016-04-24 13:08:28 +0800

    修改unsafe

--- unsafe/src/main.rs
+++ unsafe/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    //! unsafe代码块的四大用处如下：
+    //! dereferencing raw pointers
+    //! calling a function over FFI
+    //! changing types through std::cast::transmute
+    //! inline assembly（内联汇编）
+
+    let a: *const u32 = &10;
+    let b = 11i32;
+
+    unsafe {
+        assert!(*a == 10);
+        assert!(std::mem::transmute::<i32, u32>(b) == 11);
+    }
 }

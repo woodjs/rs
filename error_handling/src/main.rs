bbc5e2c7559d85155907bbe1f42ed5e0f4362b86
bugong <1761869346@qq.com>
2016-04-24 16:27:11 +0800

    修改error_handling

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     //! panic!
+
     //! 在std标准库中，存在一个枚举Option<T>
     //! Option<T>代表着两种值：
     //! Some(T): An element of type T was found
@@ -9,7 +10,28 @@ fn main() {
     //! map，可链式调用，代替嵌套match
     //! and_then，处理嵌套Option，如Option<Option<T>>，代替复杂的match
 
-    enum A {
+    //! Result<T, E>：
+    //! Ok<T>: An element T was found
+    //! Err<E>: An error was found with element E
+    //! Result<T, E>内置的方法：
+    //! unwrap
+    //! map
+    //! map_err
+    //! and_then
+
+    //! try!，与unwrap类似，不同的是，用error代替panic
+
+    //! Box<Error>
+
+    use std::num::ParseIntError;
+    use std::result;
+
+    // 定义别名
+    type Result<T> = result::Result<T, ParseIntError>;
+
+    struct A(i32);
+
+    enum B {
         A
     };
 
@@ -34,27 +56,57 @@ fn main() {
     fn test_option_and_then() {
         let opt = create_option();
 
-        opt.and_then(and_then_print);
+        // opt.and_then(and_then_print);
     }
 
-    fn and_then_print() -> Option<A> {
+    fn and_then_print() -> Option<B> {
         println!("hello, {:?} is printed by and_then!", 123);
-        Some(A::A);
+        Some(B::A)
     }
 
-    fn create_option() -> Option<A> {
-        let a = Some(A::A);
-        a
+    fn create_option() -> Option<B> {
+        Some(B::A)
+    }
+
+    fn double_number(i: &str) -> Result<i32> {
+        match i.parse::<i32>() {
+            Ok(n) => Ok(2 * n),
+            Err(e) => Err(e)
+        }
+    }
+
+    fn double_number_map(i: &str) -> Result<i32> {
+        i.parse::<i32>().map(|n| { 2 * n })
+    }
+
+    fn print_result(r: Result<i32>) {
+        match r {
+            Ok(n) => println!("current number is {}!", n),
+            Err(e) => println!("error info is : {}!", e)
+        }
     }
 
     let a = Some("b");
     // let b = None; // None值必须被使用，否则报错
 
-    test_option_unwrap(a);
+    test_option_unwrap(a); // > current value is "b"
     //test_option_unwrap(b); // 对None值unwrap，会报错
 
     test_option_map(a); // the value is b!
 
-    test_option_and_then();
+    //test_option_and_then
 
+    let a = double_number("10");
+    let b = double_number_map("20");
+    let c = double_number_map("a");
+
+    print_result(a); // current number is 20!
+    print_result(b); // current number is 40!
+    print_result(c); // error info is : invalid digit found in string!
+
+    fn test_try() -> Result<i32> {
+        Ok(1)
+    }
+
+    let d = try!(test_try());
 }

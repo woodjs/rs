dbef7e02bbcb6d8a3200ada7c6340f289f923a20
yinyanlv <yanlv.yin@gmail.com>
2016-04-27 17:50:00 +0800

    修改std_misc

--- std_misc/src/main.rs
+++ std_misc/src/main.rs
@@ -5,17 +5,28 @@ fn main() {
     //! file i/o，Result<T,io::Error>别名是io::Result<T>
     //! child process
     //! filesystem operation
-    //! program arguments
+    //! program arguments，match可用来解析简单的argument
     //! foreign function interface(FFI)
+    //! Foreign functions must be declared inside an extern block annotated with a #[link] attribute containing the name of the foreign library
+    //! this extern block links to the libm library
+    //! #[link(name = "m")]
+    //! extern {
+    //!     // this is a foreign function
+    //!     fn csqrtf(z: Complex) -> Complex;
+    //! }
+    //! 必须在unsafe代码块中调用foreign function
 
     use std::thread;
     use std::sync::mpsc;
     use std::sync::mpsc::{Sender, Receiver};
     use std::path::Path;
     use std::error::Error;
-    use std::fs::File;
+    use std::fs;
+    use std::fs::{File, OpenOptions};
+    use std::io;
     use std::io::prelude::*;
     use std::process::{Command, Stdio};
+    use std::env;
 
     // 静态类型变量，必须全部大写
     static NUM: i32 = 3;
@@ -43,6 +54,11 @@ fn main() {
     }
 
     println!("{:?}", ids); //[Ok(0), Ok(1), Ok(2)]
+
+    let args: Vec<String> = env::args().collect();
+
+    println!("my path is {}", args[0]); // my path is target\debug\std_misc.exe
+    println!("i got {} arguments: {:?}", args.len() - 1, &args[1..]); // i got 0 arguments: []
 
     let path = Path::new("out/hello_rust.txt");
 
@@ -123,5 +139,4 @@ fn main() {
                         // thread '<main>' panicked at 'couldn't spawn wc: os error', src\main.rs:114
                         // Process didn't exit successfully: `target\debug\std_misc.exe` (exit code: 101)
                     };
-
 }

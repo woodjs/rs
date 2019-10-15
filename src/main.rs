923d339c8e6fc827b842b8aa6a07e0d41315cb7b
bugong <1761869346@qq.com>
2019-10-15 23:41:42 +0800

    add: bad stack

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
-mod chore;
-
-use chore::mutex::test_mutex;
-use chore::rwlock::test_rwlock;
-use chore::refcell::test_refcell;
+mod list;
 
 fn main() {
-
-    test_mutex();
-    test_rwlock();
-    test_refcell();
+    println!("rust example!");
 }
